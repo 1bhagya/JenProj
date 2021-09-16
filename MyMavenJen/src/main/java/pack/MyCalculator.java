ee3d6ddcbff214f2c45010b4d3f12cef96bcf362
1bhagya <bhagyadoshi1@gmail.com>
2021-09-16 15:24:05 +0530

    Maven Jenkin First Try

---
+++ MyMavenJen/src/main/java/pack/MyCalculator.java
@@ -0,0 +1,14 @@
+package pack;
+
+public class MyCalculator 
+{
+	public int getSum(int x, int y)
+	{
+		return x+y;
+	}
+	public int getDiff(int x, int y)
+	{
+		return x-y;
+	}
+		
+}
---
+++ MyMavenJen/src/test/java/com/jenk/MyCalculatorTest.java
@@ -0,0 +1,29 @@
+package com.jenk;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pack.MyCalculator;
+public class MyCalculatorTest 
+{
+	@Test
+	public void test()
+	{
+		assertEquals(10,new MyCalculator().getSum(5,5));
+	}
+	@Test
+	public void test2()
+	{
+		assertEquals(5,new MyCalculator().getSum(2,3));
+	}
+	@Test
+	public void test3()
+	{
+		assertEquals(5,new MyCalculator().getDiff(10,5));
+	}
+	@Test
+	public void test4()
+	{
+		assertEquals(0,new MyCalculator().getDiff(5,5));
+	}
+}
